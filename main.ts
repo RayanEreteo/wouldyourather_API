3de50123786888bc90a13d1dc4f371179b811b4b
RayanEreteo <rayabf5@gmail.com>
2025-05-01 16:59:15 +0200

    Refactor main.ts to use environment variable for port and improve import statements

--- main.ts
+++ main.ts
@@ -1,10 +1,15 @@
-import express, {type Request, type Response} from 'express';
+import express from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
 import { addDilemma } from './controllers/dilemmaController.ts';
 
+dotenv.config();
+
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/addDilemma', addDilemma);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(process.env.PORT, () => {
+    console.log('Server is running on port ' + process.env.PORT);
 });
