5432c1ed838627be5d7a06233194b56da16102b9
RayanEreteo <rayabf5@gmail.com>
2025-05-01 17:05:23 +0200

    Update addDilemma function to validate input and return appropriate response

--- controllers/dilemmaController.ts
+++ controllers/dilemmaController.ts
@@ -1,7 +1,10 @@
 import type { Request, Response } from "express";
 
 export function addDilemma(req: Request, res: Response) {
-    res.status(200).json({
-        message: 'Dilemma added successfully',
-    })
+    const { red, blue } = req.body;
+    if (!red || !blue) {
+        res.status(400).json({ error: "Invalid input." });
+    }
+
+    res.send("test")
 }
