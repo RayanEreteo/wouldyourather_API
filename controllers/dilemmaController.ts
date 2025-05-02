4cd887465fe77de33e1c7a6b8809085a4f75b6aa
RayanEreteo <rayabf5@gmail.com>
2025-05-02 20:58:37 +0200

    Comment logic for testing

--- controllers/dilemmaController.ts
+++ controllers/dilemmaController.ts
@@ -1,10 +1,12 @@
 import type { Request, Response } from "express";
+import { connectToDatabase } from "../modules/dbConn.ts";
 
-export function addDilemma(req: Request, res: Response) {
-    const { red, blue } = req.body;
-    if (!red || !blue) {
-        res.status(400).json({ error: "Invalid input." });
-    }
+export async function addDilemma(req: Request, res: Response) {
+    // const { red, blue } = req.body;
+    // if (!red || !blue) {
+    //     res.status(400).json({ error: "Invalid input." });
+    // }
 
+    connectToDatabase()
     res.send("test")
 }
