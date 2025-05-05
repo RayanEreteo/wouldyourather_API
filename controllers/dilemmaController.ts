e8d1c1c097e60a13953f3b167cfe0e8f918b64d2
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-05-05 21:18:12 +0200

    Refactor addDilemma function to restore input validation and database insertion logic

--- controllers/dilemmaController.ts
+++ controllers/dilemmaController.ts
@@ -1,12 +1,22 @@
 import type { Request, Response } from "express";
 import { connectToDatabase } from "../modules/dbConn.ts";
 
-export async function addDilemma(req: Request, res: Response) {
-    // const { red, blue } = req.body;
-    // if (!red || !blue) {
-    //     res.status(400).json({ error: "Invalid input." });
-    // }
+export async function addDilemma(req: Request, res: Response): Promise<any> {
+    const { red, blue } = req.body;
+    if (!red || !blue) {
+        return res.status(400).json({ error: "Invalid input." });
+    }
 
-    connectToDatabase()
-    res.send("test")
+    try {
+        const con = await connectToDatabase();
+        await con?.execute(
+            "INSERT INTO dilemma (red, blue, red_click, blue_click) VALUES (?, ?, ?, ?)",
+            [red, blue, 0, 0]
+        );
+
+        return res.json({success: true, message: "Dilemma added successfully."})
+    } catch (error) {
+        console.error("Database error:", error);
+        return res.status(500).json({ error: "Internal server error." });
+    }
 }
