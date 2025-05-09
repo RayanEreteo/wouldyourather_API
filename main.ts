c9009e251b8b12a0a6eaf0809f6354438a8dc477
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-05-09 18:12:30 +0200

    Add getDilemma route to retrieve a random dilemma

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import cors from "cors"
 import express from 'express';
 import bodyParser from 'body-parser';
-import { addDilemma } from './controllers/dilemmaController.ts';
+import { addDilemma, getDilemma } from './controllers/dilemmaController.ts';
 
 const app = express();
 app.use(cors({
@@ -13,6 +13,7 @@ app.use(cors({
 app.use(bodyParser.json());
 
 app.post('/addDilemma', addDilemma);
+app.get('/getDilemma', getDilemma);
 
 app.listen(process.env.PORT, () => {
     console.log('Server is running on port ' + process.env.PORT);
